03f3b4b3240c73cf8f08e990e3ded35c9fcb6792
Adel <snakeadel00@gmail.com>
2024-05-16 10:52:22 +0300

    add shared styles file

--- shared/Input/Input.tsx
+++ shared/Input/Input.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, TextInput, TextInputProps } from 'react-native';
+import { Colors } from '../tokens';
 
 export function Input(props: TextInputProps) {
 	return (
 		<TextInput
 			style={styles.input}
-			placeholderTextColor={'#AFB2BF'}
+			placeholderTextColor={Colors.gray}
 			{...props} />
 	)
 }
@@ -12,7 +13,7 @@ export function Input(props: TextInputProps) {
 const styles = StyleSheet.create({
 	input: {
 		height: 58,
-		backgroundColor: '#2E2D3D',
+		backgroundColor: Colors.violetDark,
 		paddingHorizontal: 24,
 		borderRadius: 10,
 		fontSize: 16,
